81c1ac6b6c63cc1891dfa8d5f084c7d7b63b5e03
Your Name <you@example.com>
2025-08-28 22:04:54 +0100

    add url

--- src/features/sweets/services/sweetApi.ts
+++ src/features/sweets/services/sweetApi.ts
@@ -1,10 +1,11 @@
 export const data = [
   { name: 'Toffees', price: 1.25, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/81TLhHVZCL._AC_UF10001000_QL80_-765x765.jpg' },
-  { name: 'Sherbert Lemons', price: 2.30, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/Screenshot-2025-03-20-190721.png' },
-  { name: 'Pear Drops', price: 2.95, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/610.webp' },
-  { name: 'Butterscotch', price: 2.45, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/PARK006.png' },
+  { name: 'Sherbert Lemons', price: 2.30, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/81TLhHVZCL._AC_UF10001000_QL80_-765x765.jpg' },
+  { name: 'Pear Drops', price: 2.95, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/81TLhHVZCL._AC_UF10001000_QL80_-765x765.jpg' },
+  { name: 'Butterscotch', price: 2.34, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/PARK006.png' },
+  { name: 'Butterscotch', price: 2.54, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/PARK006.png' },
+  { name: 'Butterscotch', price: 2.1, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/PARK006.png' },
+  { name: 'Butterscotch', price: 2.67, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/PARK006.png' },
   { name: 'Cola Cubes', price: 9.00, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/BEB13-2.jpg' },
   { name: 'Peanut Brittle', price: 8.75, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/SUP32-2.png' },
-  { name: 'Rhubarb and Custards', price: 2.35, url:'https://sweetzy.co.uk/wp-content/uploads/2025/03/Barratt-Jelly-Spogs-Aniseed-Flavoured-Jelly-Buttons-Liquorice-Sweets-3.webp' },
-  { name: 'Mint Imperials', price: 4.00, url:'https://sweetzy.co.uk/wp-content/uploads/2025/01/Maoam-Fruchtkracher2-765x765.jpg' },
 ];
